19679732e4a7c02781c4cc1dd755cb352a0175b9
radhakrishnanseeman15-tech <radhakrishnanseeman15@gmail.com>
2026-04-02 14:04:52 +0530

    Updated

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,9 +3,10 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
+  const test = false;
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+  await expect(test).toBeTruthy();
 });
 
 test('get started link', async ({ page }) => {
